18e6d379d457c1eeb596686304205574c1fa7c38
Andres Achury <achuryjaramillo@gmail.com>
2025-02-16 18:18:47 -0500

    Add carousel auto-play example

--- src/app/dashboard/carousel/page.tsx
+++ src/app/dashboard/carousel/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent } from '@/components/ui/card';
 import {
   Carousel,
@@ -6,6 +8,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
 
 export default function Page() {
   return (
@@ -40,6 +43,37 @@ export default function Page() {
           loop: true, //This is for the loop option, in other words, the carousel will loop back to the first item when the last item is reached
         }}
         className='w-full max-w-sm'
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex aspect-square items-center justify-center p-6'>
+                    <span className='text-3xl font-semibold'>{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className='hidden sm:flex justify-center items-center' />
+        <CarouselNext className='hidden sm:flex justify-center items-center' />
+      </Carousel>
+
+      <p>Carousel auto-play</p>
+
+      <Carousel
+        opts={{
+          // dragFree: true, //This is for the drag free option, in other words, the carousel will not snap to the next item, snap means that the carousel will move to the next item when the user stops dragging, draggin is when the user is moving the carousel with the mouse or touch
+          loop: true, //This is for the loop option, in other words, the carousel will loop back to the first item when the last item is reached
+        }}
+        className='w-full max-w-sm'
+        plugins={[
+          Autoplay({
+            delay: 1000, //Time in milliseconds for the auto-play to change the slide
+          }),
+        ]}
       >
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
