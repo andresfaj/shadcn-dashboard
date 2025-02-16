6b1d22f7a8c34ba4c984760e7e229db14246bae8
Andres Achury <achuryjaramillo@gmail.com>
2025-02-15 21:40:57 -0500

    example of accordion

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,41 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+const listOfAccordions = [
+  {
+    id: 'item-1',
+    question: 'Is it accessible?',
+    answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+  },
+  {
+    id: 'item-2',
+    question: 'Is it responsive?',
+    answer: 'Yes. It is responsive by default.',
+  },
+  {
+    id: 'item-3',
+    question: 'Is it customizable?',
+    answer: 'Yes. It is customizable.',
+  },
+];
+
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type='single' collapsible>
+        {listOfAccordions.map((item) => {
+          return (
+            <AccordionItem value={item.id} key={item.id}>
+              <AccordionTrigger>{item.question}</AccordionTrigger>
+              <AccordionContent>{item.answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </div>
   );
 }
