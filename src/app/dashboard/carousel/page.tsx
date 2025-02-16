8bf27ea89b0e5123a36e5c02ff75a3206de7670c
Andres Achury <achuryjaramillo@gmail.com>
2025-02-16 18:04:38 -0500

    carousel example

---
+++ src/app/dashboard/carousel/page.tsx
@@ -0,0 +1,62 @@
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+export default function Page() {
+  return (
+    <div className='w-full flex justify-center flex-col items-center space-y-4'>
+      <Carousel
+        opts={{
+          // dragFree: true, //This is for the drag free option, in other words, the carousel will not snap to the next item, snap means that the carousel will move to the next item when the user stops dragging, draggin is when the user is moving the carousel with the mouse or touch
+          loop: true, //This is for the loop option, in other words, the carousel will loop back to the first item when the last item is reached
+        }}
+        className='w-full max-w-sm'
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className='md:basis-1/2'>
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex aspect-square items-center justify-center p-6'>
+                    <span className='text-3xl font-semibold'>{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className='hidden sm:flex justify-center items-center' />
+        <CarouselNext className='hidden sm:flex justify-center items-center' />
+      </Carousel>
+
+      <Carousel
+        opts={{
+          // dragFree: true, //This is for the drag free option, in other words, the carousel will not snap to the next item, snap means that the carousel will move to the next item when the user stops dragging, draggin is when the user is moving the carousel with the mouse or touch
+          loop: true, //This is for the loop option, in other words, the carousel will loop back to the first item when the last item is reached
+        }}
+        className='w-full max-w-sm'
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex aspect-square items-center justify-center p-6'>
+                    <span className='text-3xl font-semibold'>{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className='hidden sm:flex justify-center items-center' />
+        <CarouselNext className='hidden sm:flex justify-center items-center' />
+      </Carousel>
+    </div>
+  );
+}
