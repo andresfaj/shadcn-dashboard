53e1ef02c45cb4d9e3c16690c084d2c41e40ea2e
Andres Achury <achuryjaramillo@gmail.com>
2025-02-17 23:55:30 -0500

    Add date picker into the form

--- src/app/dashboard/form/page.tsx
+++ src/app/dashboard/form/page.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
 import {
   Form,
   FormControl,
@@ -16,11 +17,22 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
 
 const formSchema = z.object({
   username: z.string().min(2).max(20),
   email: z.string().email(),
   gender: z.enum(['male', 'female']),
+  dateOfBirthdate: z.date({
+    required_error: 'A date of birth is required.',
+  }),
 });
 
 export default function Page() {
@@ -113,6 +125,52 @@ export default function Page() {
                     </FormItem>
                   </RadioGroup>
                 </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* date of birth */}
+          <FormField
+            control={form.control}
+            name='dateOfBirthdate'
+            render={({ field }) => (
+              <FormItem className='flex flex-col'>
+                <FormLabel>Date of birth</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={'outline'}
+                        className={cn(
+                          'w-[240px] pl-3 text-left font-normal',
+                          !field.value && 'text-muted-foreground'
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, 'PPP')
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className='w-auto p-0' align='start'>
+                    <Calendar
+                      mode='single'
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date('1900-01-01')
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Your date of birth is used to calculate your age.
+                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
