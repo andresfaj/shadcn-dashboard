01f716c921adc771c92021dd9d9286e19c4b7ff4
Andres Achury <achuryjaramillo@gmail.com>
2025-02-17 20:23:09 -0500

    Add  left set up for toast ui component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster as SonnerToaster } from '@/components/ui/sonner';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         {children}
 
         <SonnerToaster richColors />
+        <Toaster />
       </body>
     </html>
   );
